f743158607a03792ad322fcb598c3169b4c51a89
veD-tnayrB <Brayanmc.contacto@gmail.com>
2022-11-02 22:55:40 -0400

    Fix: Has been fixed the badges

--- beyond-slate-editor/modules/home/ts/views/actions/add-badge.ts
+++ beyond-slate-editor/modules/home/ts/views/actions/add-badge.ts
@@ -10,18 +10,13 @@ export const createBadgeNode = (children: Node[]) => ({
 // Create a link and put it in the document
 export function insertBadge(editor) {
     const { selection } = editor;
- 
-    const startPoint = selection.anchor.path[0];
-    const lastPoint = selection.anchor.path[1]
-    
-    const theresOnlyOneChild = startPoint === lastPoint; 
-    let selectedChildren = [];
-    console.log(startPoint, lastPoint);
 
     const theresNoChildrensSelected = selection === null && selection.anchor === null;
     if (theresNoChildrensSelected) return;
 
+    const selectedText = Editor.string(editor, selection);
+
     // Convierte el texto seleccionado en un link usando la url pasada y el texto en si como label
-    Editor.insertNode(editor, createBadgeNode(selectedChildren));
+    Editor.insertNode(editor, createBadgeNode([{text: selectedText}]));
     Editor.insertNode(editor, createParagraphNode(), { select: true }); // Esto permite porder escribir texto luego del link
 }
